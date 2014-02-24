637598088f9b959050274eb811ab60c7d76ec502
ourangzaib khan <ourangzaib.khan.leadstech@gmail.com>
2014-02-24 14:20:22 +0500

    ourangzaib class added
    addition of new class

---
+++ feedshare/ourangzaib.h
@@ -0,0 +1,13 @@
+//
+//  ourangzaib.h
+//  feedshare
+//
+//  Created by Ourangzaib khan on 2/24/14.
+//  Copyright (c) 2014 Ourangzaib khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ourangzaib : UIView
+
+@end
